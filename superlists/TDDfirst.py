8863f63816467e04ab10e9a47f42fd4630dd0c33
harshitbahl <harshit.bahl@me.com>
2014-06-03 00:09:12 -0500

    updated the functional test cases

--- superlists/TDDfirst.py
+++ superlists/TDDfirst.py
@@ -4,11 +4,22 @@ Created on Mon Jun  2 00:57:13 2014
 
 @author: harshitbahl
 """
-def main():
-    from selenium import webdriver
-    browser = webdriver.Firefox()
-    browser.get('http://127.0.0.1:8000')
-    assert 'Django' in browser.title
+from selenium import webdriver
+import unittest
 
+class NewVistorTest(unittest.TestCase):
+    
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+    
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_later(self):
+        self.browser.get('http://127.0.0.1:8000')
+        self.assertIn('To Do', self.browser.title)
+        self.fail('finished the test!!')
+        
 if __name__ == '__main__':  
-    main()
+    unittest.main()
